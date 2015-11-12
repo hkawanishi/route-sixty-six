ab64ecb084347ad0f8e56a38f2f674aa1e05a892
Hiromi Kawanishi <contacthiromi@gmail.com>
2015-11-12 20:34:58 +0000

    set up the first page

---
+++ app/controllers/walks_controller.rb
@@ -0,0 +1,5 @@
+class WalksController < ApplicationController
+	
+	def index
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Route66::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
+  root 'walks#index'
   # root 'welcome#index'
 
   # Example of regular route:
