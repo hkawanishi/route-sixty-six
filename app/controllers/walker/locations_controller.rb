14413133adf85ce81d685f431451b44e4197f97a
Hiromi Kawanishi <contacthiromi@gmail.com>
2015-12-17 22:46:09 +0000

    generate location JSON for a map

--- app/controllers/walker/locations_controller.rb
+++ app/controllers/walker/locations_controller.rb
@@ -2,6 +2,10 @@ class Walker::LocationsController < ApplicationController
 
   def index
     @locations = Location.all
+    @hash = Gmaps4rails.build_markers(@locations) do |location, marker|
+      marker.lat location.latitude
+      marker.lng location.longitude
+    end
   end
 
   private
