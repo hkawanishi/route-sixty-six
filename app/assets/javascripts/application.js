d5ba1b3b93931a510a70d0369ea64f9ab80b4366
Hiromi Kawanishi <contacthiromi@gmail.com>
2016-01-11 05:49:34 +0000

    add jquery-datatables-rails gem

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,7 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require jquery_ujs
+//= require dataTables/jquery.dataTables
 //= require turbolinks
 //= require_tree .
 //= require underscore
